3dd4e35fe3dac6c55e1fe0b73ea40d4483eaa6f1
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-06-17 12:10:06 +0300

    add Listeners

---
+++ src/js/addBooksListeners.js
@@ -0,0 +1,12 @@
+import onBookClick from './onBookClick';
+
+export default function addBooksListeners() {
+  const booksRefs = document.querySelectorAll('.gallery-book-home-link');
+  const booksRefs2 = document.querySelectorAll('.gallery-book-link');
+  for (let i = 0; i < booksRefs.length; i++) {
+    booksRefs[i].addEventListener('click', onBookClick);
+  }
+  for (let i = 0; i < booksRefs2.length; i++) {
+    booksRefs2[i].addEventListener('click', onBookClick);
+  }
+}
