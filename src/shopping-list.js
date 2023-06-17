352845298678155312050e31e6ec82c3fbf52159
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-06-17 23:37:36 +0300

    add import

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -2,3 +2,7 @@ import './js/shoppingListService';
 import './js/donate';
 import './js/headermodal';
 import './js/light-dark-theme';
+import './js/button_up';
+import './js/footer';
+import './js/modalTeam';
+import './js/fetchBookById';
