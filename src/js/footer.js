0fb1b04339f0ec3637e9b0f97ee881836ae45dbd
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-06-18 12:45:16 -0500

    some chenging

--- src/js/footer.js
+++ src/js/footer.js
@@ -6,8 +6,27 @@ const refs = {
 };
 
 refs.openModalBtn.addEventListener('click', onOpenModal);
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.backdrop.addEventListener('click', onBackdropClick);
 
 function onOpenModal() {
-  //   document.body.classList.add('show-modal');
+  window.addEventListener('keydown', onEscKeyPress);
   refs.footer.classList.add('show-modal');
+}
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onEscKeyPress);
+  refs.footer.classList.remove('show-modal');
+}
+
+function onBackdropClick(event) {
+  if (event.target === refs.backdrop) {
+    onCloseModal();
+  }
+}
+
+function onEscKeyPress(event) {
+  if (event.key === 'Escape') {
+    onCloseModal();
+  }
 }
