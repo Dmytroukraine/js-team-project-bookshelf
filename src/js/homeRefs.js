af4f1bf7938e5819a0e896858c26942ca84aa10b
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-06-19 12:15:22 +0300

    refs need fix

--- src/js/homeRefs.js
+++ src/js/homeRefs.js
@@ -1,5 +1,10 @@
-const homeRefs = {
-  renderingContainer: document.querySelector('.js-container'),
-};
-
-export default homeRefs;
+export default function getRefs() {
+  return {
+    seeMoreBtnRef: document.querySelectorAll('.gallery-see-more-btn'),
+    galleryRef: document.querySelector('.gallery'),
+    activeCategory: document.querySelectorAll('.active'),
+    categoriesRef: document.querySelector('.nav-categories-list'),
+    bookInfo: document.querySelector('.container-modal-fav'),
+    modal: document.querySelector('[data-modal]'),
+  };
+}
