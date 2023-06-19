dcfcf837bf034ba992d8666dc0495398ab01b3e1
Dmytro-Kyianov <dkyianov@gmail.com>
2023-06-19 23:50:39 +0300

    header.css, l/d theme

--- src/js/light-dark-theme.js
+++ src/js/light-dark-theme.js
@@ -1,40 +1,8 @@
-
-
-// document.querySelector('#theme-switch-toggle').addEventListener('click', (event) => {
-
-//     event.preventDefault();
-//     if (localStorage.getItem('theme') ==='dark') {
-//         localStorage.removeItem('theme');
-//     }
-//     else { 
-//         localStorage.setItem('theme', 'dark');
-//     }
-//     addDarkClassToHTML(); 
-// });
-
-// function addDarkClassToHTML(){
-//     try {
-//         if (localStorage.getItem('theme') ==='dark') {
-//             document.querySelector('body').classList.add('dark');
-//             document.querySelector('header').classList.add('dark-header');
-           
-//         }
-//         else {
-//             document.querySelector('body').classList.remove('dark');
-//             document.querySelector('header').classList.remove('dark-header');
-//             // document.getElementById('.theme-switch__marker').checked = true;
-//         }
-//     } catch(error) { console.log(error.message);
-//     }
-// }
-
-// addDarkClassToHTML();
-
 const inputTheme = document.querySelector('.theme-switch__control');
 const spanTheme = document.querySelector('.span-theme');
-const body = document.querySelector('body');
 const logoIcon = document.querySelector('.header-logo-icon');
 const logoIcon1 = document.querySelector('.header-logo-icon1');
+const body = document.querySelector('body');
 let indexTheme = false;
 
 inputTheme.addEventListener('change', () => {
