5b36e656701001fafc565253e4c2f956d8ead1b7
Janne57 <evgeniyahome1981@gmail.com>
2023-06-20 16:12:13 +0300

    mobile menu

--- src/js/light-dark-theme.js
+++ src/js/light-dark-theme.js
@@ -17,12 +17,14 @@ function addDarkClassToHTML(){
         if (localStorage.getItem('theme') ==='dark') {
             document.querySelector('body').classList.add('dark');
             document.querySelector('header').classList.add('dark-header');
-           
+            document.querySelector('.header-mobile-menu').classList.add('dark-header');
+            document.querySelector('#theme-switch-toggle').setAttribute('checked', true);
         }
         else {
             document.querySelector('body').classList.remove('dark');
             document.querySelector('header').classList.remove('dark-header');
-            // document.getElementById('.theme-switch__marker').checked = true;
+            document.querySelector('.header-mobile-menu').classList.remove('dark-header');
+            document.getElementById('.theme-switch__marker').checked = true;
         }
     } catch(error) { console.log(error.message);
     }
