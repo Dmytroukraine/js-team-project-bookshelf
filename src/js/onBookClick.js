57d15290c103647acb24a2a66e91a06972b41488
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-06-20 10:33:59 +0300

    Update onBookClick.js

--- src/js/onBookClick.js
+++ src/js/onBookClick.js
@@ -16,50 +16,45 @@ const addBookBtn = document.querySelector('.fav-add-book-btn');
 const addBookNotif = document.querySelector('.add-book-notification');
 
 async function onBookClick(event) {
-    
-    const dataId = event.currentTarget.dataset.id;
-    const book = await fetchBookById(event.currentTarget.dataset.id);
+  const dataId = event.currentTarget.dataset.id;
+  const book = await fetchBookById(event.currentTarget.dataset.id);
+  if (loadFromLocalStorage(dataId) == null) {
+    addBookNotif.classList.add('hidden');
+    addBookBtn.textContent = 'add to shopping list';
+  } else {
+    addBookNotif.classList.remove('hidden');
+    addBookBtn.textContent = 'remove from the shopping list';
+  }
+  renderModalMarkup(book);
+  FavModal();
+
+  console.log(booksArray);
+  addBookBtn.addEventListener('click', onAddBookBtn);
+
+  function onAddBookBtn() {
     if (loadFromLocalStorage(dataId) == null) {
-        
-        addBookNotif.classList.add('hidden');
-        addBookBtn.textContent = 'add to shopping list';
+      booksArray.push(book);
+      const dataJSON = JSON.stringify(booksArray);
+      localStorage.setItem('books', dataJSON);
+
+      addBookNotif.classList.remove('hidden');
+      addBookBtn.textContent = 'remove from the shopping list';
     } else {
-        addBookNotif.classList.remove('hidden');
-        addBookBtn.textContent = 'remove from the shopping list';
+      addBookNotif.classList.add('hidden');
+      addBookBtn.textContent = 'add to shopping list';
+
+      const index = booksArray.findIndex((book) => book._id === dataId);
+      if (index !== -1) {
+        booksArray.splice(index, 1);
+        localStorage.setItem('books', JSON.stringify(booksArray));
+        Notiflix.Notify.info('Book removed from shopping list');
+      }
     }
-    renderModalMarkup(book);
-    FavModal();
-  
-console.log(booksArray)
-    addBookBtn.addEventListener('click', onAddBookBtn);
-
-    function onAddBookBtn() {
-        if (loadFromLocalStorage(dataId) == null) {
-            // saveToLocalStorage(book);
-
-            booksArray.push(book);
-            const dataJSON = JSON.stringify(booksArray);
-            localStorage.setItem('books', dataJSON);
-
-          addBookNotif.classList.remove('hidden');
-          addBookBtn.textContent = 'remove from the shopping list';
-        } else {
-            // removingBookFromShoppingList(dataId);
-            addBookNotif.classList.add('hidden');
-            addBookBtn.textContent = 'add to shopping list';
-
-            const index = booksArray.findIndex(book => book._id === dataId);
-            if (index !== -1) {
-                booksArray.splice(index, 1);
-                localStorage.setItem('books', JSON.stringify(booksArray));
-                booksArray = [];
-                Notiflix.Notify.info('Book removed from shopping list');
-            }
-
-        }
-    }
-    
+  }
 }
+
+export { onBookClick };
+
 
 
 
