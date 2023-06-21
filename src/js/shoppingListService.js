169638f5c7fe0bf8080f0caaa9b2b9a18be18d64
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-06-21 03:35:16 +0300

    up css update js

--- src/js/shoppingListService.js
+++ src/js/shoppingListService.js
@@ -38,31 +38,36 @@ function renderingShoppingList() {
       booksList.insertAdjacentHTML(
         'beforeend',
         `<div class="shopping-list-thumb">
+        <div class="cover-shopping-list" style="background-image: url('${book.book_image}'); background-size: cover;">
+          </div>
+          <div class="book-interface">
+          <div class="book-title-btn">
+          <div>
+          <h2 class="shopping-list-book-title">${book.title}</h2>
+            <p class="shopping-list-book-category">${book.list_name}</p>
+            </div>
           <button class="delete-shopping-list-btn" type="button" data-id="${book._id}">
             <svg class="delete-shopping-list-icon">
               <use href="${trash}"></use>
             </svg>
           </button>
-          <div class="cover-shopping-list" style="background-image: url('${book.book_image}'); background-size: cover;">
           </div>
-          <div class="book-interface">
-            <h2 class="shopping-list-book-title">${book.title}</h2>
-            <p class="shopping-list-book-category">${book.list_name}</p>
             <p class="shopping-list-book-about">${book.description}</p>
+            <div class="shopping-list-book">
             <p class="shopping-list-book-author">${book.author}</p>
             <ul class="shopping-list-trading">
               <li class="shopping-list-trading-item">
-                <a class="shopping-list-trading-link brightness" href="${book.buy_links[0].url}" target="_blank">
+                <a class="shopping-list-trading-link brightness" href="${book.buy_links[0].url}" target="_blank" rel="noopener noreferrer">
                   <img src="${amazon}" class="shopping-list-trading-icon-amazon" alt="Amazon icon" />
                 </a>
               </li>
               <li class="shopping-list-trading-item">
-                <a class="shopping-list-trading-link" href="${book.buy_links[1].url}" target="_blank">
+                <a class="shopping-list-trading-link" href="${book.buy_links[1].url}" target="_blank" rel="noopener noreferrer">
                   <img src="${appleBooks}" class="shopping-list-trading-icon-apple-books" alt="Apple icon" />
                 </a>
               </li>
               <li class="shopping-list-trading-item">
-                <a class="shopping-list-trading-link" href="${book.buy_links[2].url}" target="_blank">
+                <a class="shopping-list-trading-link" href="${book.buy_links[2].url}" target="_blank" rel="noopener noreferrer">
                   <img src="${bookShop}" class="shopping-list-trading-icon-book-shop" alt="Book shop icon" />
                 </a>
               </li>
